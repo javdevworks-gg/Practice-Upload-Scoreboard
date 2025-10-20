40e919dc67d4e1d928a879c1aa2a7fb9ac262399
Javi <jav.devworks@gmail.com>
2025-10-19 18:21:06 -0700

    Commit initial files
    First of many!

---
+++ index.js
@@ -0,0 +1,16 @@
+let homeScore = document.getElementById("home-score")
+let score = 0
+
+function incrementBy1(){
+    score +=1
+    homeScore.textContent = score
+}
+function incrementBy2(){
+    score +=2
+    homeScore.textContent = score
+}
+
+function incrementBy3(){
+    score +=3
+    homeScore.textContent = score
+}
